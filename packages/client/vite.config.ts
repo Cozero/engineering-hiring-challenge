556595585edccf3ad593733a34c4068fbd5474a7
Nuno Carvalhão <nuno.carvalhao@gmail.com>
2023-07-03 14:14:12 +0100

    refactor(client): Add less support

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,69 +1,34 @@
-import { join } from 'path';
-import { readFileSync } from 'fs';
-import { ResolvedConfig, UserConfigExport, defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-import { peerDependencies } from './package.json';
-
-const DEV_SERVER_PORT = 8000;
-
-// Commit information to identify the build
-function getBuildId(): string {
-  try {
-    const versionPath = join(__dirname, '..', '..', 'VERSION');
-    const versionData = readFileSync(versionPath)
-      .toString()
-      .split(/[\r\n]+/);
-    const [, branch] = versionData
-      .find(line => line.includes('GIT_BRANCH'))!
-      .split('=');
-    const [, shortHash] = versionData
-      .find(line => line.includes('GIT_SHORT_HASH'))!
-      .split('=');
-    return `${shortHash}@${branch}`;
-  } catch (err) {
-    console.log(err);
-    return 'no-git';
-  }
-}
+import * as path from 'path';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import vitePluginImp from 'vite-plugin-imp';
 
 // https://vitejs.dev/config/
-export default ({
-  command,
-}: Pick<ResolvedConfig, 'command' | 'mode'>): UserConfigExport => {
-  const baseConfig = defineConfig({
-    define: {
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      __REQUIRED_SERVER_VERSION__: `'${peerDependencies['@cozero/server']}'`,
-    },
-    resolve: {
-      alias: {
-        '~': join(__dirname, 'src'),
-      },
-    },
-    plugins: [react()],
-  });
+export default defineConfig({
+  server: { port: 8080 },
+  plugins: [
+    react(),
+    vitePluginImp({
+      libList: []
+    })
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true
+      }
+    }
+  },
+  define: { 'process.env': process.env },
 
-  if (command === 'serve') {
-    return {
-      ...baseConfig,
-      server: {
-        port: DEV_SERVER_PORT,
-      },
-    };
+  optimizeDeps: {
+    include: ['tailwind.config.js']
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      'tailwind.config.js': path.resolve(__dirname, 'tailwind.config.js')
+    }
   }
-
-  const assetsDir = 'assets';
-
-  return {
-    ...baseConfig,
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        output: {
-          entryFileNames: join(assetsDir, `[name].${getBuildId()}.[hash].js`),
-        },
-      },
-    },
-  };
-};
+});
