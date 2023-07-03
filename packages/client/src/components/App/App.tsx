c00a12db272542e6d544678230a7694201b9b472
Nuno Carvalhão <nuno.carvalhao@gmail.com>
2023-07-03 16:38:28 +0100

    feature(client): Add metrics table

--- packages/client/src/components/App/App.tsx
+++ packages/client/src/components/App/App.tsx
@@ -1,33 +1,105 @@
-import React, { FC, useEffect, useState } from 'react';
-import { getApiUrl } from '../../utils';
+import React, { useEffect, useState } from 'react';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import { Button, Layout, Menu, theme } from 'antd';
+import { Metric } from '@cozero/lib';
+import Table, { ColumnsType } from 'antd/es/table';
+import { API_URL } from '../../config';
+import { Logger } from '../../utils';
 
-export const App: FC<unknown> = () => {
-  const [isBackendAlive, setIsBackendAlive] = useState(false);
-  const [isBackendAliveLoaded, setIsBackendAliveLoaded] = useState(false);
+const { Header, Sider, Content } = Layout;
+
+export const App: React.FC = () => {
+  const [response, setResponse] = useState<Metric[]>([]);
+
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
 
   useEffect(() => {
-    if (isBackendAliveLoaded) {
-      return;
+    async function fetchResponse(): Promise<void> {
+      try {
+        const res = await fetch(`${API_URL}/reports/metrics?company=Company1`);
+        const data = await res.json();
+        setResponse(data);
+      } catch (err) {
+        Logger.error(err);
+      }
     }
 
-    const loadIsAlive = async (): Promise<void> => {
-      const isAliveResponse = await fetch(`${getApiUrl}/ping`);
+    fetchResponse();
+  }, []);
 
-      setIsBackendAliveLoaded(true);
-      setIsBackendAlive(isAliveResponse.ok);
-    };
+  const columns: ColumnsType<Metric> = [
+    {
+      title: 'Organization Name',
+      dataIndex: 'organizationName',
+      key: 'id',
+      render: (organizationName: string) => <p>{organizationName}</p>,
+    },
+    {
+      title: 'Emissions',
+      dataIndex: 'emissions',
+      key: 'id',
+      render: (emissions: number) => <p>{emissions}</p>,
+    },
+  ];
 
-    loadIsAlive();
-  }, [
-    isBackendAlive,
-    isBackendAliveLoaded,
-    setIsBackendAliveLoaded,
-    setIsBackendAlive,
-  ]);
-
-  if (!isBackendAliveLoaded) {
-    return <div>Loading...</div>;
-  }
-
-  return <div>Can reach backend: {isBackendAlive ? 'Yes' : 'No'}</div>;
+  return (
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: 'Home',
+              label: 'home',
+              icon: <UserOutlined rev={''} />,
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Button
+            type="text"
+            icon={
+              collapsed ? (
+                <MenuUnfoldOutlined rev={''} />
+              ) : (
+                <MenuFoldOutlined rev={''} />
+              )
+            }
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          <Table
+            loading={!response?.length}
+            columns={columns}
+            dataSource={response}
+          />
+        </Content>
+      </Layout>
+    </Layout>
+  );
 };
