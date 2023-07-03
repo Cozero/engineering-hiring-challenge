0cbd384259c90dc462b289d85b9fd676f94f631f
Nuno Carvalhão <nuno.carvalhao@gmail.com>
2023-07-03 16:37:28 +0100

    feature(server): Add metrics enpoints

--- packages/server/src/modules/reports/reports.controller.ts
+++ packages/server/src/modules/reports/reports.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common'
+import { PrismaService } from '../../prisma.service'
+import { Metric } from '@cozero/lib'
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  @Get('metrics')
+  async getMetrics(@Query('company') companyName: string): Promise<Metric[]> {
+    return this.prismaService.metric.findMany({
+      where: { organizationName: companyName },
+      take: 1000,
+      orderBy: { reportedAt: 'desc' },
+    })
+  }
+}
